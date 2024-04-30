3e99083efcf6fa6fcb4ce13f7f6b32de35d401e8
ronan-X <142173292+ronan-X@users.noreply.github.com>
2024-04-30 11:03:04 -0300

    Update sonder.js

--- sonder.js
+++ sonder.js
@@ -8,6 +8,8 @@ function abriraba() {
 
 function fecharaba() {
   var abriraba = document.querySelector('.pesquisar-aba-tudo')
+  var fecharaba = document.querySelector('#fechar-aba')
+  fecharaba.style.display = 'none'
 
   abriraba.style.display = 'none'
 }
@@ -15,19 +17,22 @@ function fecharaba() {
 function botaocamisa1() {
   var camisapopular = document.querySelector('#camisa-popular-imagem')
 
-  camisapopular.src = 'imagens/images__57_-removebg-preview.png'
+  camisapopular.src =
+    'https://github.com/ronan-X/calculator-/assets/142173292/af4154ac-c83c-4ac8-9a46-0125604fb82e'
 }
 
 function botaocamisa2() {
   var camisapopular = document.querySelector('#camisa-popular-imagem')
 
-  camisapopular.src = 'imagens/images__56_-removebg-preview.png'
+  camisapopular.src =
+    'https://github.com/ronan-X/calculator-/assets/142173292/67e65ce3-d235-4adc-88e5-a8b1d79b9d34'
 }
 
 function botaocamisa3() {
   var camisapopular = document.querySelector('#camisa-popular-imagem')
 
-  camisapopular.src = 'imagens/images__58_-removebg-preview.png'
+  camisapopular.src =
+    'https://github.com/ronan-X/calculator-/assets/142173292/3dd1fb89-1e6a-4ef7-a244-4adc02782d45'
 }
 
 function drop1() {
